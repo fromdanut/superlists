f8143e9f7afebd3b196fbeadf1bfb6fa3296ce39
fromdanut <remidelannoy@hotmail.com>
2017-09-20 18:28:39 +0200

    Add the needed element into the homepage template

--- functionnal_test.py
+++ functionnal_test.py
@@ -19,10 +19,10 @@ class NewVisitorTest(unittest.TestCase):
         # She notices the page title and header mention to-do lists
         self.assertIn('To-Do', self.browser.title)
         header_text = self.browser.find_element_by_tag_name('h1').text
-        self.assertIn('To-do', header_text)
+        self.assertIn('To-Do', header_text)
 
         # She is invited to enter a to-do item
-        inputbox = self.brower.find_element_by_id('id_new_item')
+        inputbox = self.browser.find_element_by_id('id_new_item')
         self.assertEqual(
             inputbox.get_attribute('placeholder'),
             'Enter a to-do item'
@@ -38,7 +38,8 @@ class NewVisitorTest(unittest.TestCase):
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
         self.assertTrue(
-            any(row.text == '1: Buy peacock feathers' for row in rows)
+            any(row.text == '1: Buy peacock feathers' for row in rows),
+            "New item doesn't appear in table"
         )
 
         # Reminder
