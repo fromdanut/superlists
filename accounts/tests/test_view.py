6aba77ceb2ee207d28c21d3d61c7f1497238d0e8
fromdanut <remidelannoy@hotmail.com>
2017-09-29 14:15:05 +0200

    Implements logout with FT + UT

--- accounts/tests/test_view.py
+++ accounts/tests/test_view.py
@@ -21,7 +21,7 @@ class SendLoginEmailViewTest(TestCase):
         self.assertEqual(mock_send_mail.called, True)
         (subject, body, from_email, to_list), kwargs = mock_send_mail.call_args
         self.assertEqual(subject, 'Your login link for Superlists')
-        self.assertEqual(from_email, 'noreply@superlists')
+        self.assertEqual(from_email, 'noreply@rdlnn.org')
         self.assertEqual(to_list, ['edith@mail.com'])
 
     def test_create_token_associated_with_email(self):
--- functional_test/test_login.py
+++ functional_test/test_login.py
@@ -36,12 +36,22 @@ class LoginTest(FunctionalTest):
         url = url_search.group(0)
         self.assertIn(self.live_server_url, url)
 
-        # she clicks it
+        # She clicks it
         self.browser.get(url)
 
-        # she is logged in!
+        # She is logged in!
         self.wait_for(
             lambda: self.browser.find_element_by_link_text('Log out')
         )
         navbar = self.browser.find_element_by_css_selector('.navbar')
         self.assertIn(TEST_EMAIL, navbar.text)
+
+        # Now she logs out
+        self.browser.find_element_by_link_text('Log out').click()
+
+        # She is logged out
+        self.wait_for(
+            lambda: self.browser.find_element_by_name('email')
+        )
+        navbar = self.browser.find_element_by_css_selector('.navbar')
+        self.assertNotIn(TEST_EMAIL, navbar.text)
